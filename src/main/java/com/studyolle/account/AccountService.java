70f4c58d3d0d4e8e8d171fc1fda123e00a78f36e
kimhayoung <qlalzl9@naver.com>
2020-11-17 19:37:45 +0900

    Update profile image

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -90,6 +90,7 @@ public class AccountService implements UserDetailsService {
         account.setUrl(profile.getUrl());
         account.setOccupation(profile.getOccupation());
         account.setLocation(profile.getLocation());
+        account.setProfileImage(profile.getProfileImage());
         accountRepository.save(account);
     }
 }
