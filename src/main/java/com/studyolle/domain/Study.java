4a2a0d06d15386f25c4223e0058dd2930fc8a06f
kimhayoung <qlalzl9@naver.com>
2020-11-27 19:09:56 +0900

    Study test

--- src/main/java/com/studyolle/domain/Study.java
+++ src/main/java/com/studyolle/domain/Study.java
@@ -78,4 +78,8 @@ public class Study {
     public boolean isManager(UserAccount userAccount) {
         return this.managers.contains(userAccount.getAccount());
     }
+
+    public void addMember(Account account) {
+        this.members.add(account);
+    }
 }
