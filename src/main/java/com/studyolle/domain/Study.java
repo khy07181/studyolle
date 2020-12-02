a19a9e51ac0f620d829dc6079c5a5286722cf48d
kimhayoung <qlalzl9@naver.com>
2020-12-02 21:52:19 +0900

    Study join and leave

--- src/main/java/com/studyolle/domain/Study.java
+++ src/main/java/com/studyolle/domain/Study.java
@@ -4,6 +4,8 @@ import com.studyolle.account.UserAccount;
 import lombok.*;
 
 import javax.persistence.*;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -21,6 +23,8 @@ import java.util.Set;
         @NamedAttributeNode("managers")})
 @NamedEntityGraph(name = "Study.withManagers", attributeNodes = {
         @NamedAttributeNode("managers")})
+@NamedEntityGraph(name = "Study.withMembers", attributeNodes = {
+        @NamedAttributeNode("members")})
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
 @Builder @AllArgsConstructor @NoArgsConstructor
@@ -91,6 +95,10 @@ public class Study {
         this.members.add(account);
     }
 
+    public void removeMember(Account account) {
+        this.getMembers().remove(account);
+    }
+
     public String getImage() {
         return image != null ? image : "/images/default_banner.jpg";
     }
@@ -137,5 +145,9 @@ public class Study {
 
     public boolean isRemovable() {
         return !this.published; // TODO 모임을 했던 스터디는 삭제할 수 없다.
+    }
+
+    public String getEncodedPath() {
+        return URLEncoder.encode(this.path, StandardCharsets.UTF_8);
     }
 }
