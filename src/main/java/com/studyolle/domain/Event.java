44932053caf2266eef3a392789354570bbff1902
kimhayoung <qlalzl9@naver.com>
2020-12-08 17:17:56 +0900

    Event list view

--- src/main/java/com/studyolle/domain/Event.java
+++ src/main/java/com/studyolle/domain/Event.java
@@ -9,6 +9,10 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@NamedEntityGraph(
+        name = "Event.withEnrollments",
+        attributeNodes = @NamedAttributeNode("enrollments")
+)
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
 public class Event {
@@ -79,5 +83,9 @@ public class Event {
             }
         }
         return false;
+    }
+
+    public int numberOfRemainSpots() {
+        return this.limitOfEnrollments - (int) this.enrollments.stream().filter(Enrollment::isAccepted).count();
     }
 }
