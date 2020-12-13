e78af8af3f549ca9e8b387aaa017fddefc9593d8
kimhayoung <qlalzl9@naver.com>
2020-12-13 19:03:21 +0900

    Event notifications

---
+++ src/main/java/com/studyolle/modules/event/EnrollmentEvent.java
@@ -0,0 +1,13 @@
+package com.studyolle.modules.event;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public abstract class EnrollmentEvent {
+
+    protected final Enrollment enrollment;
+    protected final String message;
+
+}
